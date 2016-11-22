f59ef65b7d1c37d7e1226480e337a639a44611f3
Yuriy Zhloba <y_zhloba@wargaming.net>
2016-11-22 19:06:10 +0300

    erlang/bbmustache implementation

---
+++ erl_bbmustache/src/mustache_bm.erl
@@ -0,0 +1,33 @@
+-module(mustache_bm).
+
+-export([main/1]).
+
+main(Args) ->
+    io:format("main ~p~n", [Args]),
+    case Args of
+        [DataFile, BindingsFile, NumTests | _] ->
+            do_test(
+                atom_to_list(DataFile),
+                atom_to_list(BindingsFile),
+                list_to_integer(atom_to_list(NumTests))
+            );
+        _ -> io:format("invalid arguments~n"),
+            init:stop(1)
+    end,
+    ok.
+
+do_test(DataFile, BindingsFile, NumTests) ->
+    io:format("do_test ~p ~p ~p~n", [DataFile, BindingsFile, NumTests]),
+    {ok, Data} = file:read_file(DataFile),
+    {ok, Bindings} = file:read_file(BindingsFile),
+    Bindings2 = jiffy:decode(Bindings, [return_maps]),
+
+    T1 = erlang:system_time(milli_seconds),
+    [test(Data, Bindings2) || _ <- lists:seq(0, NumTests)],
+    T2 = erlang:system_time(milli_seconds),
+    io:format("~p~n", [T2 - T1]),
+    init:stop().
+
+
+test(Data, Bindings) ->
+    bbmustache:render(Data, Bindings, [{key_type, binary}]).
