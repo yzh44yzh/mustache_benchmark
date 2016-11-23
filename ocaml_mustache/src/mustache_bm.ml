d89b177031317ae6b77a5638c276ca8885324a4c
Yuriy Zhloba <y_zhloba@wargaming.net>
2016-11-23 09:39:43 +0300

    ocaml implementation

---
+++ ocaml_mustache/src/mustache_bm.ml
@@ -0,0 +1,33 @@
+let rec do_tests (template : string) (bindings : Ezjsonm.t) (num : int) : unit =
+  if num == 0 then ()
+  else
+    begin
+      Mustache.render (Mustache.of_string template) bindings
+      |> ignore;
+      do_tests template bindings (num - 1)
+    end
+
+
+let load_file f =
+  let ic = open_in f in
+  let n = in_channel_length ic in
+  let s = Bytes.create n in
+  really_input ic s 0 n;
+  close_in ic;
+  (s)
+
+
+let usage () =
+  print_endline "Usage: $0 template_file bindings_file"
+
+
+let () =
+  match Sys.argv with
+  | [| _ ; template_file; bindings_file; num_tests |] ->
+      let template = load_file template_file in
+      let bindings = Ezjsonm.from_string (load_file bindings_file) in
+      let t1 = Unix.gettimeofday() in
+      do_tests template bindings (int_of_string num_tests);
+      let t2 = Unix.gettimeofday() in
+      print_endline (string_of_float (t2 -. t1))
+  | _ -> usage ()
